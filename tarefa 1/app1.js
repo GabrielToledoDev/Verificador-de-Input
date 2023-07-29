82cee2481b2c5ad7518b0c2a7ec2c06bef1fe7a1
Gabriel Toledo (Chase) <103535213+GabrielToledoDev@users.noreply.github.com>
2023-07-29 11:39:27 -0300

    Add files via upload

---
+++ tarefa 1/app1.js
@@ -0,0 +1,17 @@
+const buttonElement = document.getElementById('checkButton');
+buttonElement.addEventListener('click', function() {
+
+const verificarItem = document.getElementById('numeroInput');
+console.log(verificarItem);
+    
+    if (verificarItem.value < 8 ) {
+        verificarItem.classList.add('button-red');
+        verificarItem.classList.remove('button-green');
+        
+    } else {
+        
+        verificarItem.classList.add('button-green');
+        verificarItem.classList.remove('button-red');
+       
+    }
+})
